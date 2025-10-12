50efbd920416371c726d15c11b61c9876d3d61ba
adi960280-cmd <adityamalhotra960@gmail.com>
2025-10-13 00:00:00 +0530

    config.py

--- config.py
+++ config.py
@@ -9,7 +9,8 @@ class Config(object):
     pass
     """
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    API_ID = int(os.environ["API_ID", ]
-    API_HASH = os.environ["API_HASH", ""]
-    AUTH_USERS = """"
+    API_ID = int(os.environ["API_ID", 29115102]
+    API_HASH = os.environ["API_HASH", "1a331db2b00e9d2decaa9c7276449eb6"]
+    AUTH_USERS = "1224092270"""
  
+
