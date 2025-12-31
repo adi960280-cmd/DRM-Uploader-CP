2edaf1d9281d79777b51b687672289d5ecf1b09c
adi960280-cmd <adityamalhotra960@gmail.com>
2025-12-31 19:30:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,9 +8,10 @@ class Config(object):
     # get a token from @BotFather
     pass
     """
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7998257759:AAFGOPn-12Mkjtz4uTbuEDNMkM9jNRRjK9c")
     API_ID = int(os.environ["API_ID", 29115102]
     API_HASH = os.environ["API_HASH", "1a331db2b00e9d2decaa9c7276449eb6"]
     AUTH_USERS = "1224092270"""
  
+
 
